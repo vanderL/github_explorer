fa8b23f678f00861bc18cb7b57cd4031e107ba05
Vander Lima <vnd.vander@gmail.com>
2021-12-15 23:43:41 -0300

    Styling Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,66 @@
 import React from "react";
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
 import { Link } from 'react-router-dom';
-import { Title } from "./styles";
+import { Title, Form, Repository } from "./styles";
 
 const Dashboard: React.FC = () => {
   return (
-    <div>
+    <>
+      <img src={logoImg} alt="Github Explorer" />
       <Title> Você ta no Dashboard</Title>
-      <Link to="/repository" > Repository </Link>
-    </div>
+
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repository>
+        <Link to="/repository" >
+          <img src="https://avatars.githubusercontent.com/u/12676148?v=4" alt="Vander" />
+          <div>
+            <strong>vanderL/github-explorer</strong>
+            <p>Testing react with typescript</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+
+        <Link to="/repository" >
+          <img src="https://avatars.githubusercontent.com/u/12676148?v=4" alt="Vander" />
+          <div>
+            <strong>vanderL/github-explorer</strong>
+            <p>Testing react with typescript</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+
+        <Link to="/repository" >
+          <img src="https://avatars.githubusercontent.com/u/12676148?v=4" alt="Vander" />
+          <div>
+            <strong>vanderL/github-explorer</strong>
+            <p>Testing react with typescript</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+
+        <Link to="/repository" >
+          <img src="https://avatars.githubusercontent.com/u/12676148?v=4" alt="Vander" />
+          <div>
+            <strong>vanderL/github-explorer</strong>
+            <p>Testing react with typescript</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Repository>
+
+
+    </>
   )
 }
 
