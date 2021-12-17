2daeb5ac86b2e253be5e66f842aa03297975112e
Vander Lima <vnd.vander@gmail.com>
2021-12-17 16:15:45 -0300

    Styling repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,17 +1,62 @@
 import React from "react";
 import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Header, Issues, RepositoryInfo } from "./styles";
 
 interface RepositoryParam {
   repository: string;
-
 }
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParam>();
   return (
     <div>
-      <h1> Você ta no Repository: {params.repository}</h1>
-      <Link to="/"> Dashboard </Link>
+      <>
+        <Header>
+          <img src={logoImg} alt="Github Explorer" />
+          <Link to="/">
+            <FiChevronLeft size={16} />
+            Voltar
+          </Link>
+        </Header>
+
+        <RepositoryInfo>
+          <header>
+            <img src="https://avatars.githubusercontent.com/u/12676148?v=4" alt="avatar" />
+            <div>
+              <strong>Vander</strong>
+              <p>Descrição</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>1800</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>48</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>67</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+
+        <Issues>
+          <Link to={`/repositories/`} >
+            <div>
+              <strong>aklçaklçak</strong>
+              <p>kalçakçakç</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </Link>
+        </Issues>
+      </>
     </div>
   )
 }
